3b971b28419b66b13fffc9584b5ad49a761718b6
tanyaleepr <tanyaleepr@gmail.com>
2026-03-17 21:10:41 -0400

    renamed

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/security-portfolio/security-portfolio/src/assets",
+  base: "/security-portfolio",
   plugins: [react()],
 });
