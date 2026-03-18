f11b269621921d656e934b3989b84684f46983d8
tanyaleepr <tanyaleepr@gmail.com>
2026-03-17 20:54:50 -0400

    adding vite

--- security-portfolio/vite.config.ts
+++ security-portfolio/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/tanya-portfolio/",
+  base: "/security-portfolio/security-portfolio",
   plugins: [react()],
 });
